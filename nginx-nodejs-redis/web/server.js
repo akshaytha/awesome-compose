4ecc5f7a9079b8d1d8ca40960fd4e9d01160ee0b
akshaytha <127379591+akshaytha@users.noreply.github.com>
2025-04-02 14:10:32 +0530

    Update server.js
    Signed-off-by: akshaytha <127379591+akshaytha@users.noreply.github.com>

--- nginx-nodejs-redis/web/server.js
+++ nginx-nodejs-redis/web/server.js
@@ -7,12 +7,32 @@ const redisClient = redis.createClient({
   port: 6379
 });
 
+// MySQL Connection
+const db = mysql.createConnection({
+  host: 'mysql',  // This is the service name in docker-compose.yml
+  user: 'myuser',
+  password: 'mypassword',
+  database: 'mydatabase'
+});
+
+db.connect(err => {
+  if (err) {
+    console.error('MySQL Connection Error:', err);
+  } else {
+    console.log('Connected to MySQL');
+  }
+});
+
 app.get('/', function(req, res) {
     redisClient.get('numVisits', function(err, numVisits) {
         numVisitsToDisplay = parseInt(numVisits) + 1;
         if (isNaN(numVisitsToDisplay)) {
             numVisitsToDisplay = 1;
         }
+      // Insert visit count into MySQL
+        db.query('INSERT INTO visits (count) VALUES (?)', [numVisitsToDisplay], (err, result) => {
+            if (err) console.error(err);
+        });
        res.send(os.hostname() +': Number of visits is: ' + numVisitsToDisplay);
         numVisits++;
         redisClient.set('numVisits', numVisits);
